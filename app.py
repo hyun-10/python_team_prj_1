b98e3f28dcd7494cb1483fdbab277ed6b3a6ab58
hyun-10 <111376655+hyun-10@users.noreply.github.com>
2022-10-11 20:26:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,26 @@
 import streamlit as st
-import FinanceDataReader as fdr
-nasdaq_df=fdr.DataReader('IXIC','2020')
-st.write(nasdaq_df)
+from Make_page import make_page
+from pages import project1 as p1
+from pages import project2 as p2
+from pages import project3 as p3
+from pages import project4 as p4
+from pages import project5 as p5
+from pages import project6 as p6
+from pages import project7 as p7
+from pages import project8 as p8
+
+app = make_page()
+
+# 타이틀
+st.title('Project')
+
+# 추가할 페이지
+app.add_page("lstm 란?", p1.app)
+app.add_page("pykrx 소개", p2.app)
+app.add_page("FinanceDataReader소개", p3.app)
+app.add_page("FinanceDataReader를 활용한 최근 경제 흐름 분석", p4.app)
+app.add_page("현재 시장 현황", p5.app)
+app.add_page("원하는 주식 예측 데이터 만들기", p6.app)
+app.add_page("backtrader 소개", p7.app)
+app.add_page("backtrader 활용한 모의투자 해보기", p8.app)
+app.run()
